f639f682d2356daafc5799e34b865598786b99dd
SoczuKS <soczynskik@gmail.com>
2025-01-07 01:56:21 +0100

    Client - Start screen

--- Client/src/main/java/mpks/jabia/Main.java
+++ Client/src/main/java/mpks/jabia/Main.java
@@ -2,14 +2,47 @@ package mpks.jabia;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 public class Main extends GameApplication {
+    private final int windowWidth = 1360;
+
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setWidth(1024);
-        settings.setHeight(768);
+        settings.setWidth(windowWidth);
+        settings.setHeightFromRatio(16.0 / 9.0);
         settings.setTitle("Jabia");
         settings.setVersion("1.0.0");
+    }
+
+    @Override
+    protected void initUI() {
+        TextArea usernameTextArea = new TextArea();
+        usernameTextArea.setPrefWidth(250);
+        usernameTextArea.setPrefHeight(50);
+        usernameTextArea.setPromptText("Username");
+
+        TextArea passwordTextArea = new TextArea();
+        passwordTextArea.setPrefWidth(250);
+        passwordTextArea.setPrefHeight(50);
+        passwordTextArea.setPromptText("Password");
+
+        Button loginButton = new Button("Login");
+        loginButton.setPrefWidth(100);
+        loginButton.setPrefHeight(50);
+        loginButton.setOnAction(e -> {
+            String username = usernameTextArea.getText();
+            String password = passwordTextArea.getText();
+            System.out.println("Username: " + username + ", Password: " + password);
+        });
+
+        int uiX = windowWidth / 2 - 125;
+
+        FXGL.addUINode(usernameTextArea, uiX, 100);
+        FXGL.addUINode(passwordTextArea, uiX, 175);
+        FXGL.addUINode(loginButton, uiX + 75, 250);
     }
 
     public static void main(String[] args) {
